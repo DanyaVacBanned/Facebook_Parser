aee7eff0a7ec12a2336542095fab9bd08d42fe25
Danil <90012018+DanyaVacBanned@users.noreply.github.com>
2022-12-04 14:12:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ username = 'username'
 password = 'password'
 #Авторизация
 client = Client(username, password)
-link_and_text = parser.main()
-client.send(message='Есть совпадение по вашему запросу: {link_and_text}', thread_id='USER_ID', thread_type=ThreadType.USER)
+with open('file_with_urls.txt', 'r', ecnoding='utf-8') as f:
+  for i in f.read().strip().split():
+    post_result = parser.parsing(i)
+    client.send(message=f'Есть совпадение по вашему запросу: {post_result}', thread_id='USER_ID', thread_type=ThreadType.USER)
 
 #id 100002069809245
 client.logout()
